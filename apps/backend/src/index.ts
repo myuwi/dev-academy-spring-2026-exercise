7289b4344bc29ea1128c0a3dc8bf061783ee3bc6
myuwi <myuwi@proton.me>
2026-01-10 14:38:27 +0200

    feat: add basic request logging

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import stats from "./routes/stats";
 
 const app = new Hono();
 
+app.use(logger());
 app.route("/stats", stats);
 
 export default app;
