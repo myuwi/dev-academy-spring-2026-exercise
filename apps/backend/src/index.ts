b0e758fcc2261c6888d4fe21bbefe74549f334a5
myuwi <myuwi@proton.me>
2026-01-18 14:23:53 +0200

    feat: handle backend shutdown signals gracefully

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -7,4 +7,13 @@ const app = new Hono();
 app.use(logger());
 app.route("/stats", stats);
 
+// Handle shutdown gracefully
+const shutdown = async () => {
+  console.info("Shutting down server...");
+  process.exit(0);
+};
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+
 export default app;
